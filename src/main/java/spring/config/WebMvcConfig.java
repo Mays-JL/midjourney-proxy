ac47db4e9e58953bce10820a64f58f606ab6c28e
linlin20ai <3357929671@qq.com>
2025-04-29 09:57:30 +0800

    FIX： 解决跨域和连接超时问题

--- src/main/java/spring/config/WebMvcConfig.java
+++ src/main/java/spring/config/WebMvcConfig.java
@@ -3,7 +3,9 @@ package spring.config;
 import cn.hutool.core.text.CharSequenceUtil;
 import com.github.novicezk.midjourney.ProxyProperties;
 import com.github.novicezk.midjourney.support.ApiAuthorizeInterceptor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -17,6 +19,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	@Resource
 	private ProxyProperties properties;
 
+	@Value("${CROS_ALLOWED_ORIGINS}")
+	private String[] allowedOrigins;
+
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/").setViewName("redirect:doc.html");
@@ -28,6 +33,16 @@ public class WebMvcConfig implements WebMvcConfigurer {
 			registry.addInterceptor(this.apiAuthorizeInterceptor)
 					.addPathPatterns("/submit/**", "/task/**", "/account/**");
 		}
+	}
+
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins(allowedOrigins) // 允许的前端域名
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+				.allowedHeaders("*")
+				.allowCredentials(true); // 如果需要发送 cookies，设置为 true
 	}
 
 }
